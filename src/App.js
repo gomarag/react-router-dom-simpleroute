acfccb57ad8d7a93c11807426373b90e1d8aaed0
gomarag <maragriver@gmail.com>
2021-09-16 12:17:49 +0900

    react router DOM을 사용한 SPA 구현 환경 세팅

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
+import React from 'react';
+// 컴포넌트를 꺼내오기 위한 import 구문
+import { BrowserRouter, Switch, Route, Link } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <main>
+        <Home />
+        <MyPage />
+        <Dashboard />
+      </main>
     </div>
   );
 }
