dbc4b071376dc16083515c04f9d6d31bfcfc831c
gomarag <maragriver@gmail.com>
2021-09-16 13:57:28 +0900

    Router로 컴포넌트별 SPA 구현

--- src/App.js
+++ src/App.js
@@ -6,14 +6,55 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <main>
-        <Home />
-        <MyPage />
-        <Dashboard />
-      </main>
-    </div>
+    // Router 사용을 위한 설정
+    <BrowserRouter>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              {/* Link는 주어진 경로로 이동 */}
+              {/* <a>태그는 새로고침으로 페이지를 처음부터 렌더링, <Link>는 SPA */}
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/mypage">MyPage</Link>
+            </li>
+            <li>
+              <Link to="/dashboard">Dashboard</Link>
+            </li>
+          </ul>
+        </nav>
+        {/* 경로 매칭을 위한 Switch와 Route로 <Home/>, <MyPage/>,<Dashboard/> 컴포넌트 연결 */}
+        {/* Switch는 위에서 아래로 경로를 차례로 실행. exact속성은 주어진 경로와 완전 일치를 검사 */}
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/mypage">
+            <MyPage />
+          </Route>
+          <Route path="/dashboard">
+            <Dashboard />
+          </Route>
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
+}
+
+// Home 컴포넌트
+function Home() {
+  return <h1>Home</h1>;
+}
+
+// MyPage 컴포넌트
+function MyPage() {
+  return <h1>MyPage</h1>;
+}
+
+// Dashboard 컴포넌트
+function Dashboard() {
+  return <h1>Dashboard</h1>;
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+// Router 컴포넌트 사용을 위한 import
+// 새로고침 없이 주소를 변경할 수 있도록 함
+import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <BrowserRouter><App /></BrowserRouter>
+  ,document.querySelector('#root')
 );
 
 // If you want to start measuring performance in your app, pass a function
